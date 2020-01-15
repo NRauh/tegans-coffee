528ee43d30d420acde09e76a48e1fd5d3aef5b93
Tegan Rauh <NRauh@users.noreply.github.com>
2020-01-15 15:02:05 -0600

    Move post title to own component

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -5,24 +5,21 @@ const gatsby = jest.requireActual('gatsby');
 module.exports = {
   ...gatsby,
   graphql: jest.fn(),
-  Link: jest.fn().mockImplementation(
-    // these props are invalid for an `a` tag
-    ({
-      activeClassName,
-      activeStyle,
-      getProps,
-      innerRef,
-      partiallyActive,
-      ref,
-      replace,
-      to,
-      ...rest
-    }) =>
-      React.createElement('a', {
-        ...rest,
-        href: to,
-      })
-  ),
+  Link: ({
+    activeClassName,
+    activeStyle,
+    getProps,
+    innerRef,
+    partiallyActive,
+    ref,
+    replace,
+    to,
+    ...rest
+  }) =>
+    React.createElement('a', {
+      ...rest,
+      href: to,
+    }),
   StaticQuery: jest.fn(),
   useStaticQuery: jest.fn(),
 };
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,30 +1,7 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import ThemeColors from '../utils/theme';
 import { rhythm } from '../utils/typography';
-
-const PostTitle = props => {
-  const { titleTag: TitleTag, link, children, center } = props;
-
-  let content;
-
-  if (link) {
-    content = <Link to={link}>{children}</Link>;
-  } else {
-    content = children;
-  }
-
-  const titleStyles = {
-    marginBottom: rhythm(0.5),
-    color: ThemeColors.link,
-  };
-
-  if (center) {
-    titleStyles.textAlign = 'center';
-  }
-
-  return <TitleTag css={titleStyles}>{content}</TitleTag>;
-};
+import PostTitle from './post-title';
 
 const PostHeader = props => {
   const {
